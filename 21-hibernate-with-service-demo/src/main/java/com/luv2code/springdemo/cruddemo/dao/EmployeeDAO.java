26700d4f31e9a78b12fd0cb17ccc6ea7cf924400
Artyom Zhuravliov <artiom25454@gmail.com>
2020-07-14 17:48:23 +0300

    All my Spring Boot prjocets

---
+++ 21-hibernate-with-service-demo/src/main/java/com/luv2code/springdemo/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo.cruddemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springdemo.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
---
+++ 23-spring-data-jpa-crud-demo/src/main/java/com/luv2code/springdemo/cruddemo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo.cruddemo.service;
+
+import java.util.List;
+
+import com.luv2code.springdemo.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void delteById(int theId);
+}
